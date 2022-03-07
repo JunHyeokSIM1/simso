78c0f3eff767c3cca31030491d1df5b32c0ba225
simso <wnsgur765z@gmail.com>
2022-03-07 15:52:14 +0900

    feat: UserService update
    Dto 변경

--- src/main/java/com/simso/service/UserService.java
+++ src/main/java/com/simso/service/UserService.java
@@ -2,6 +2,9 @@ package com.simso.service;
 
 import com.simso.domain.Item;
 import com.simso.domain.User;
+import com.simso.dto.UserResponseDto;
+import com.simso.dto.UserSaveRequestDto;
+import com.simso.dto.UserUpdateRequestDto;
 import com.simso.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,16 +22,16 @@ public class UserService {
 
         this.userRepository = userRepository;
     }
-    public Long register(User user){
+    public Long register(UserSaveRequestDto requestDto){
 
-        getValidateDuplicateUser(user);
-        userRepository.save(user);
-        return user.getId();
+        getValidateDuplicateUser(requestDto);
+
+        return userRepository.save(requestDto.toEntity()).getId();
     }
 
 
-    private  void getValidateDuplicateUser(User user){
-        userRepository.findByname(user.getUsername())
+    private  void getValidateDuplicateUser(UserSaveRequestDto requestDto){
+        userRepository.findByname(requestDto.getUsername())
                 .ifPresent(m ->{
                     throw new IllegalStateException("user 이름이 중복됩니다");
                 });
@@ -48,4 +51,20 @@ public class UserService {
     }
 
 
+    public Long update(Long id, UserUpdateRequestDto requestDto) {
+        User user = userRepository.findByid(id)
+                .orElseThrow(()->
+                        new IllegalArgumentException("해당 id가 없습니다.id=" + id));
+
+        user.update(requestDto.getUsername(), requestDto.getPassword());
+        return id;
+    }
+
+    public UserResponseDto findById(Long id){
+        User user = userRepository.findByid(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("해당 id가 없습니다.id=" + id));
+
+        return new UserResponseDto(user);
+    }
 }
