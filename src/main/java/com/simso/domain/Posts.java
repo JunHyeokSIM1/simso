8d1612dfaabbd9a960e3d7b36bac0e2676d89c96
JunHyeok SIM <73094907+neuruSim@users.noreply.github.com>
2022-03-17 11:47:26 +0900

    feat: Post 서비스 (#2)
    * add: post entity
    
    * add: posts dto
    
    PostsResponseDto
    PostsSaveRequestDto
    PostsUpdateRequestDto
    
    * add: post controller
    
    Repository, Serive
    
    * add: posts service test

---
+++ src/main/java/com/simso/domain/Posts.java
@@ -0,0 +1,43 @@
+package com.simso.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="title")
+    @NotNull
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    @NotNull
+    private String content;
+
+    @Column(name = "author")
+    @NotNull
+    private String author;
+
+    @Builder
+    public Posts(Long id, String title, String content, String author) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
