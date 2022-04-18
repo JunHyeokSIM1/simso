b5ce82d1a7ac4ce459ac6edf123564751a65cb48
nero <wnsgur765z@gmail.com>
2022-04-18 13:13:30 +0900

    test: RoadmapSerive test code 작성

--- src/test/java/com/simso/service/RoadmapServiceTest.java
+++ src/test/java/com/simso/service/RoadmapServiceTest.java
@@ -4,16 +4,20 @@ import com.simso.domain.Roadmap;
 import com.simso.domain.Role;
 import com.simso.domain.User;
 import com.simso.dto.UserResponseDto;
+import com.simso.dto.roadmap.RoadmapResponseDto;
 import com.simso.dto.roadmap.RoadmapSaveRequestDto;
 import com.simso.repository.RoadmapRepository;
 import com.simso.repository.UserRepository;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RoadmapServiceTest {
@@ -24,6 +28,11 @@ class RoadmapServiceTest {
     RoadmapRepository roadmapRepository;
     @Autowired
     UserRepository userRepository;
+
+    @AfterEach
+    public void cleanup() {
+        roadmapRepository.deleteAll();
+    }
 
     @DisplayName("로드맵 기본필드를 등록한다")
     @Test
@@ -40,19 +49,39 @@ class RoadmapServiceTest {
 
         //then
         Optional<Roadmap> findByRoadmap = roadmapRepository.findById(register);
-        Assertions.assertThat(register).isEqualTo(findByRoadmap.orElse(null).getId());
+        assertThat(register).isEqualTo(findByRoadmap.orElse(null).getId());
 
     }
+    @DisplayName("등록된 로드맵을 조회한다")
+    @Test
+    public void findRoadmapList() {
+        //given
+        User user = createUser();
+        Optional<User> findUser = userRepository.findById(user.getId());
+        UserResponseDto responseDto = new UserResponseDto(findUser.orElse(null));
 
+        RoadmapSaveRequestDto requestDto = createRoadmapRequestDto(responseDto);
+        RoadmapSaveRequestDto requestDto2 = createRoadmapRequestDto(responseDto);
+        roadmapService.register(requestDto);
+        roadmapService.register(requestDto2);
+
+        //when
+        List<RoadmapResponseDto> findAll = roadmapService.findAllDesc();
+
+        //then
+        assertThat(findAll.size()).isEqualTo(2);
+    }
+
+    // 로드맵 create save Request dto
     private RoadmapSaveRequestDto createRoadmapRequestDto(UserResponseDto responseDto) {
         return RoadmapSaveRequestDto.builder()
                 .title("테스트 타이틀")
                 .content("테스트 본문")
-                .userResponseDto(responseDto)
+                .userId(responseDto.getId())
                 .build();
     }
 
-
+    // user create save Request dto
     private User createUser() {
         return userRepository.save(User.builder()
                 .username("빌더테스트")
