c3f7c69c62416f4350988288b88ea42825cc0c33
nero <wnsgur765z@gmail.com>
2022-04-26 11:42:06 +0900

    feat: Add errorResponse

--- src/main/java/com/simso/global/exception/ErrorResponse.java
+++ src/main/java/com/simso/global/exception/ErrorResponse.java
@@ -3,28 +3,62 @@ package com.simso.global.exception;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ErrorResponse {
-    private LocalDateTime now;
+    private String message;
     private int status;
     private List<FieldError> errors;
     private String code;
 
-    public ErrorResponse(LocalDateTime now) {
-        this.now = now;
+    public ErrorResponse(ErrorCode code) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.code = code.getCode();
+    }
+
+    public ErrorResponse(ErrorCode code, List<FieldError> errors) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.errors = errors;
+        this.code = code.getCode();
+    }
+
+    public static ErrorResponse of(ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+
+    public static ErrorResponse of(ErrorCode code, BindingResult bindingResult) {
+        return new ErrorResponse(code, FieldError.of(bindingResult));
     }
 
     @Getter
-    @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     private static class FieldError {
-        private final String field;
-        private final String value;
-        private final String reason;
+
+        private String field;
+        private String value;
+        private String reason;
+
+        public FieldError(String field, String value, String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(BindingResult bindingResult) {
+            List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
     }
 }
