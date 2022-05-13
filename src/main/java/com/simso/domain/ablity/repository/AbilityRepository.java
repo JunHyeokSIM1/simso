bf37be393c88bba7eb7f54fc61fdd0cd46853be7
nero <wnsgur765z@gmail.com>
2022-05-13 10:03:47 +0900

    feat: Add Abilty  entity , repository

---
+++ src/main/java/com/simso/domain/ablity/repository/AbilityRepository.java
@@ -0,0 +1,7 @@
+package com.simso.domain.ablity.repository;
+
+import com.simso.domain.ablity.entity.Ability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AbilityRepository extends JpaRepository<Ability, Long> {
+}
